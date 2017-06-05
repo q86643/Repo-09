e55ca5ca6638d20b195f3ad8e5ebed14df3c5c8d
q86643 <q86643@gmail.com>
2017-06-05 19:50:33 +0800

    Add files via upload

---
+++ LAB09-checkpoint2.ino
@@ -0,0 +1,50 @@
+#include <LiquidCrystal.h> // include the library code
+LiquidCrystal lcd(12, 7, 5, 4, 3,13); // initialize interface pins
+
+const byte ledPin=10;
+const byte TrigPin = 8;
+const byte EchoPin = 9;
+long duration;
+
+void setup()
+{
+Serial.begin(9600);
+pinMode(TrigPin,OUTPUT);
+pinMode(EchoPin,INPUT);
+digitalWrite(ledPin,HIGH);
+attachInterrupt(0,doEncoder,LOW);
+}
+
+void loop(){
+if(LOW){
+digitalWrite(ledPin,HIGH);
+}
+else{
+digitalWrite(ledPin,LOW);
+}
+}
+
+void doEncoder() 
+{
+  long cm;
+digitalWrite(TrigPin,LOW);
+delayMicroseconds(2);
+digitalWrite(TrigPin,HIGH);
+delayMicroseconds(10);
+digitalWrite(TrigPin,LOW);
+delayMicroseconds(2);
+duration = pulseIn(EchoPin,HIGH);
+cm = duration/58;
+if(cm>0)
+{
+  delay(2000);
+  Serial.print( "Distance = ");
+  Serial.print(cm);
+  Serial.println("cm");
+   lcd.clear();
+   lcd.print("Distance = ");
+   lcd.print(cm);
+}
+
+} 
+
---
+++ LAB09-checkpoint3.ino
@@ -0,0 +1,48 @@
+#include <LiquidCrystal.h> // include the library code
+LiquidCrystal lcd(12, 7, 5, 4, 3,13); // initialize interface pins
+
+const byte ledPin=10;
+const byte TrigPin = 8;
+const byte EchoPin = 9;
+long duration;
+
+void setup()
+{
+Serial.begin(9600);
+pinMode(TrigPin,OUTPUT);
+pinMode(EchoPin,INPUT);
+digitalWrite(ledPin,HIGH);
+attachInterrupt(0,doEncoder,LOW);
+}
+
+void loop(){
+  noInterrupts();
+digitalWrite(ledPin,HIGH);
+digitalWrite(ledPin,LOW);
+  interrupts();
+}
+
+void doEncoder() 
+{
+  long cm;
+digitalWrite(TrigPin,LOW);
+delayMicroseconds(2);
+digitalWrite(TrigPin,HIGH);
+delayMicroseconds(10);
+digitalWrite(TrigPin,LOW);
+delayMicroseconds(2);
+duration = pulseIn(EchoPin,HIGH);
+cm = duration/58;
+if(cm>0)
+{
+  delay(2000);
+  Serial.print( "Distance = ");
+  Serial.print(cm);
+  Serial.println("cm");
+   lcd.clear();
+   lcd.print("Distance = ");
+   lcd.print(cm);
+}
+
+} 
+
